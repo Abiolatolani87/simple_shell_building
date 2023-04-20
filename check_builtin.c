a0f3922573090f3666ffc4d1110a86935d50144e
Emmanuel Adje Frimpong <tydevelopes@gmail.com>
2023-04-20 01:25:38 +0000

    try fixing betty error

--- check_builtin.c
+++ check_builtin.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * built-in - checks whether command is a built in
+ * built_in - checks whether command is a built in
  * @str: string to be checked
- Return: 1 if yes, 0 if no
+ * Return: 1 if yes, 0 if no
 */
 
 int built_in(char *str, char **builtin)
