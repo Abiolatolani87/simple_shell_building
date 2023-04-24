f877ba92d45e9700a289c4ea25c1a538b33168f3
Emmanuel Adje Frimpong <tydevelopes@gmail.com>
2023-04-24 11:48:48 +0000

    Fix betty errors

--- intprt_dollar.c
+++ intprt_dollar.c
@@ -1,18 +1,10 @@
 #include "main.h"
 
-/*1. Tokenise
-2. check the first character of each  token for $
-    2.1  if it has $ sign (check length = 2)
-            check next char
-                if = ?
-                    (replace $? with exit status)
-                 else if ($)
-                    (replace $$ with pid)
-    2.2  else != 2 call getenv()
-            (replace getenv with value/result)
-
+/**
+ * interpret_dollar - interprets dollar sign
+ * @str: pointer to strings
+ * @status: exit code
 */
-
 void interpret_dollar(char **str, int status)
 {
 	char *new_str = NULL;
