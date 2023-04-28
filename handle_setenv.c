7f47b0c3e8c944872aed573843725e7b69f039cd
Emmanuel Adje Frimpong <tydevelopes@gmail.com>
2023-04-28 04:12:40 +0000

    fixed errors

--- handle_setenv.c
+++ handle_setenv.c
@@ -11,17 +11,13 @@ void handle_setenv(int argv_count, char **argv, int *status)
 	if (argv_count != 3)
 	{
 		/*fprintf(stderr, "%s:usage: %s VARIABLE VALUE\n", argv[0], argv[0]);*/
-		newputs(argv[0]);
-		newputs(":usage: ");
-		newputs(argv[0]);
-		_puts(" VARIABLE VALUE");
 		*status = 127;
 	}
 	else
 	{
 		if ((setenv(argv[1], argv[2], 1)) == -1)
 		{
-			_puts("command invoked cannot execute");
+			/*_puts("command invoked cannot execute");*/
 			*status = 126;
 		}
 	}
